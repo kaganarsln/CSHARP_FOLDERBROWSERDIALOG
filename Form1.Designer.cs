3e25bf62ff31a3d429db9daa8cb7afc3933022af
kaganarsln <kaan.arslangorurr@gmail.com>
2024-11-10 19:41:10 +0300

    CSHARP_FOLDERBROWSERDIALOG

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace FOLDERBROWSERDİALOG1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.olustur = new System.Windows.Forms.Button();
+            this.yolsec = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ileri = new System.Windows.Forms.Button();
+            this.geri = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resmigoster = new System.Windows.Forms.Button();
+            this.klasorsec = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.olustur);
+            this.groupBox1.Controls.Add(this.yolsec);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(58, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(825, 228);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BELGE OLUŞTURMA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Dosya Adı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Dosya Adı:";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(628, 119);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 20);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "pptx";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(628, 89);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(50, 20);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "xlsx";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(628, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 20);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "docx";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // olustur
+            // 
+            this.olustur.Location = new System.Drawing.Point(466, 118);
+            this.olustur.Name = "olustur";
+            this.olustur.Size = new System.Drawing.Size(127, 38);
+            this.olustur.TabIndex = 3;
+            this.olustur.Text = "OLUŞTUR";
+            this.olustur.UseVisualStyleBackColor = true;
+            this.olustur.Click += new System.EventHandler(this.olustur_Click);
+            // 
+            // yolsec
+            // 
+            this.yolsec.Location = new System.Drawing.Point(466, 51);
+            this.yolsec.Name = "yolsec";
+            this.yolsec.Size = new System.Drawing.Size(127, 40);
+            this.yolsec.TabIndex = 2;
+            this.yolsec.Text = "YOLU SEÇ";
+            this.yolsec.UseVisualStyleBackColor = true;
+            this.yolsec.Click += new System.EventHandler(this.yolsec_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(205, 118);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(242, 38);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 51);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 40);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ileri);
+            this.groupBox2.Controls.Add(this.geri);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.resmigoster);
+            this.groupBox2.Controls.Add(this.klasorsec);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(58, 246);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(825, 278);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "KLASÖR İÇERİK LİSTELEME";
+            // 
+            // ileri
+            // 
+            this.ileri.Location = new System.Drawing.Point(718, 106);
+            this.ileri.Name = "ileri";
+            this.ileri.Size = new System.Drawing.Size(69, 55);
+            this.ileri.TabIndex = 6;
+            this.ileri.Text = ">";
+            this.ileri.UseVisualStyleBackColor = true;
+            this.ileri.Click += new System.EventHandler(this.ileri_Click);
+            // 
+            // geri
+            // 
+            this.geri.Location = new System.Drawing.Point(418, 106);
+            this.geri.Name = "geri";
+            this.geri.Size = new System.Drawing.Size(75, 55);
+            this.geri.TabIndex = 5;
+            this.geri.Text = "<";
+            this.geri.UseVisualStyleBackColor = true;
+            this.geri.Click += new System.EventHandler(this.geri_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(499, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(213, 205);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "YOL:";
+            // 
+            // resmigoster
+            // 
+            this.resmigoster.Location = new System.Drawing.Point(205, 203);
+            this.resmigoster.Name = "resmigoster";
+            this.resmigoster.Size = new System.Drawing.Size(159, 23);
+            this.resmigoster.TabIndex = 2;
+            this.resmigoster.Text = "RESMİ GÖSTER";
+            this.resmigoster.UseVisualStyleBackColor = true;
+            this.resmigoster.Click += new System.EventHandler(this.resmigoster_Click);
+            // 
+            // klasorsec
+            // 
+            this.klasorsec.Location = new System.Drawing.Point(27, 203);
+            this.klasorsec.Name = "klasorsec";
+            this.klasorsec.Size = new System.Drawing.Size(144, 23);
+            this.klasorsec.TabIndex = 1;
+            this.klasorsec.Text = "KLASÖR SEÇ";
+            this.klasorsec.UseVisualStyleBackColor = true;
+            this.klasorsec.Click += new System.EventHandler(this.klasorsec_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(370, 164);
+            this.listBox1.TabIndex = 0;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.ımageList1.ImageSize = new System.Drawing.Size(150, 150);
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(898, 536);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button olustur;
+        private System.Windows.Forms.Button yolsec;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button resmigoster;
+        private System.Windows.Forms.Button klasorsec;
+        private System.Windows.Forms.Button ileri;
+        private System.Windows.Forms.Button geri;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FOLDERBROWSERDİALOG1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string dosyaAd, dosyaYol, dosyaFormat;
+       
+
+        private void klasorsec_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Description = "Bir Klasör Seçiniz.";
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string seciliklasor = folderBrowserDialog1.SelectedPath;
+                DirectoryInfo dizinbilgi = new DirectoryInfo(seciliklasor);
+                FileSystemInfo[] dosyabilgi = dizinbilgi.GetFileSystemInfos();
+
+                foreach (FileSystemInfo dosya in dosyabilgi)
+                {
+                    if (dosya is DirectoryInfo)
+                    {
+                        listBox1.Items.Add("Klasör: " + dosya.Name);
+
+                    }
+                    else if (dosya is FileInfo)
+                    {
+                        listBox1.Items.Add("Dosya: " + dosya.Name);
+
+                    }
+                }
+                label3.Text = seciliklasor;
+                dosyaYol = seciliklasor;
+
+                
+            }
+
+        }
+
+        private void resmigoster_Click(object sender, EventArgs e)
+        {
+            string[] dosyaresim = Directory.GetFiles(dosyaYol, "*.jpg");
+            foreach(string dosya in dosyaresim) 
+            {
+                Image resim = Image.FromFile(dosya);
+                ımageList1.Images.Add(resim);
+            }
+            if (ımageList1.Images.Count > 0) 
+            {
+                pictureBox1.Image = ımageList1.Images[0];
+            }
+        }
+        int gecis = 0;
+        private void geri_Click(object sender, EventArgs e)
+        {
+            gecis--;
+            if (gecis < 0) 
+            {
+                gecis = ımageList1.Images.Count - 1;
+            }
+            pictureBox1.Image = ımageList1.Images[gecis];
+            
+        }
+
+        private void ileri_Click(object sender, EventArgs e)
+        {
+            gecis++;
+            if(gecis == ımageList1.Images.Count) 
+            {
+                gecis = 0;
+            }
+            pictureBox1.Image = ımageList1.Images[gecis];
+        }
+
+        private void olustur_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text)) 
+            {
+                dosyaAd = textBox2.Text;
+
+                if (radioButton1.Checked) 
+                {
+                    dosyaFormat = ".docx";
+                }
+                else if (radioButton2.Checked) 
+                {
+                    dosyaFormat = ".xlsx";
+                }
+                else if (radioButton3.Checked) 
+                {
+                    dosyaFormat = ".pptx";
+                }
+                if (!string.IsNullOrEmpty(dosyaFormat)) 
+                {
+                    StreamWriter sw = File.CreateText(dosyaYol + "\\" + dosyaAd + dosyaFormat);
+                    sw.Close();
+                    MessageBox.Show("Dosyanız Başarıyla Oluşturuldu!", "Bilgilendirme Satırıdır.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else 
+                {
+                    MessageBox.Show("Bir Yol Seçmelisiniz!");
+                }
+            }
+        }
+
+        private void yolsec_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                dosyaYol = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = dosyaYol;
+            }
+        }
+    }
+}
